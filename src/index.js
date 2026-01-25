138a920a9367424e576b0749205ae1e683ea8982
jospeh-barraza <barraza.josesph@gmail.com>
2026-01-25 16:04:46 -0300

    conneccion-front

--- src/index.js
+++ src/index.js
@@ -18,7 +18,8 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/public', express.static('public'));
+app.use('/img', express.static('public/img'));
+
 
 
 // rutas
@@ -29,23 +30,6 @@ app.use('/aquaticWorld/categoria',categoriaRoutes);
 app.use('/aquaticWorld',productosRoutes)
 
 app.use('/aquaticWorld', cursosRouters);
-
-
-// en index.js o donde tengas las rutas
-import poolPostgres from './config/dB.postgres.js';
-
-app.get('/aquaticWorld/productos-test', async (req, res) => {
-  try {
-    const { rows } = await poolPostgres.query(
-      'SELECT * FROM productos ORDER BY id LIMIT 10'
-    );
-    res.json({ ok: true, data: rows });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ ok: false, error: error.message });
-  }
-});
-
 
 
 
