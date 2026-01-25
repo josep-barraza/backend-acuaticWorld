a14688fd998ca8e259a5f8b88b181c269cef179f
jospeh-barraza <barraza.josesph@gmail.com>
2026-01-24 22:58:35 -0300

    db-conectada

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ const app = express();
 
 // middlewares globales
 app.use(cors({
- origin: process.env.FRONTEND_URL,
+   origin: process.env.FRONTEND_URL,
+  credentials: true,
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -30,30 +31,8 @@ app.use('/aquaticWorld',productosRoutes)
 app.use('/aquaticWorld', cursosRouters);
 
 
-app.get('/', (req, res) => {
-  res.send('Backend funcionando 🚀');
-});
 
-import poolPostgres from './config/dB.postgres.js';
 
-app.get('/test-db', async (req, res) => {
-  try {
-    console.log('🟡 Probando conexión DB...');
-    const result = await poolPostgres.query('SELECT 1 AS test');
-    console.log('🟢 Conexión OK');
 
-    res.json({
-      ok: true,
-      result: result.rows,
-    });
-  } catch (error) {
-    console.error('🔴 ERROR REAL DB:', error);
-    res.status(500).json({
-      ok: false,
-      error: error.message,
-      stack: error.stack,
-    });
-  }
-});
 
 export default app;
